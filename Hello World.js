41246b90ca3f3c47ee6a918616c0df8dd0df03a2
LeonShimazaki <48808986+LeonShimazaki@users.noreply.github.com>
2019-12-13 19:04:16 +1300

    Create Hello World.js

---
+++ Hello World.js
@@ -0,0 +1,15 @@
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('root')
+);
+
+{/*Javascript Commenting*/}
+
+{/* 
+Multiline commenting same as Javascript Commenting
+Watch for: Curly brace wrapped!!
+*/}
+
+{/*h1 is just html*/}
+
+{/* document.getElementById('root') Javascript*/}
