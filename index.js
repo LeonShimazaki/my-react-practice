acf4dfa6d2dab8c1078d7a36a04fb7749e9ba5ab
LeonShimazaki <48808986+LeonShimazaki@users.noreply.github.com>
2019-12-18 23:47:18 +1300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+// From scratch, initialize the React app
+// Render an <App /> component
+// Create the <App /> component from scratch
+// Have the <App /> component render 3 or 4 checkboxes with paragraphs or spans next to it
+// like you're making a todo list with some hard-coded items on it
+
 import React from "react"
 import ReactDom from "react-dom"
 
@@ -5,14 +11,32 @@ import Navbar from "./Navbar"
 import MainContent from "./MainContent"
 import Footer from "./Footer"
 
+const firstName = "Leon"
+const lastName = "Shimazaki"
+
+
 function App() {
-  return(
-    <div>
-        <nav><Navbar/></nav>
-        <MainContent/>
-        <Footer/>
-    </div>
-  )
+    const date = new Date()
+    const hours = date.getHours()
+    let timeOfDay
+    if (hours < 12) {
+        timeOfDay = "morning"
+    }
+    if (hours > 12 && hours < 17) {
+        timeOfDay = "afternoon"
+    }
+    if (hours > 17) {
+        timeOfDay = "nighttime"
+    }
+    return(
+        <div>
+            <Navbar/>
+            <h1>Hello {`${firstName} ${lastName}`}</h1>
+            <h1>Good {timeOfDay}</h1>
+            <MainContent/>
+            <Footer/>
+        </div>
+    )
 }
 
 export default App
